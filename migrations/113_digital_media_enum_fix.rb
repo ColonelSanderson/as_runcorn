7ecb8935cce6a2eabcd2e014e39c8d6c1df26959
Sean Anderson <sean.anderson@gaiaresources.com.au>
2020-03-20 19:40:09 +0800

    * Fix enum capitalisation

--- migrations/113_digital_media_enum_fix.rb
+++ migrations/113_digital_media_enum_fix.rb
@@ -4,7 +4,7 @@ Sequel.migration do
   up do
     self.transaction do
       old_enum_id = self[:enumeration_value][:value => 'Digital Media - Other'][:id]
-      new_enum_id = self[:enumeration_value][:value => 'Digital media - Other'][:id]
+      new_enum_id = self[:enumeration_value][:value => 'Digital Media - Other'][:id]
 
       self[:digital_representation].filter(:contained_within_id => old_enum_id).update(:contained_within_id => new_enum_id)
       self[:enumeration_value].filter(:id => old_enum_id).delete
